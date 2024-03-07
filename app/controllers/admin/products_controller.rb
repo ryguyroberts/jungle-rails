70bf32b7ac82ffe6775a5f33343c70b8f877c51a
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-07 13:24:59 -0800

    add basic security to admin pages and env variables

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+
   def index
     @products = Product.order(id: :desc).all
   end
