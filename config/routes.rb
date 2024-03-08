a61220e861df8b6c820297b9920a2b705b9b140c
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-07 17:40:32 -0800

    add register/login/logout buttons. conditional to user logged in

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,13 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :create, :new]
   end
 
-  
+  # Login logout routes
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
+  post 'users' => 'users#create'
   
 
   # The priority is based upon order of creation: first created -> highest priority.
