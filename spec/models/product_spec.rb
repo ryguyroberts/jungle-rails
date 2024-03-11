ed7465d61bda98dcfb4eda202583bab9560a4423
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-11 10:11:35 -0700

    update product test, add name validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,5 +15,17 @@ RSpec.describe Product, type: :model do
       expect(product.valid?).to be_truthy
     end
 
+    it 'validates presence of name' do
+      product = Product.new(price: 10, quantity: 5, category: Category.new)
+
+      # Test the false path first (No name) which should be required by our model
+      expect(product.valid?).to be_falsey
+      expect(product.errors[:name]).to include("can't be blank")
+
+      # Test after valid name
+      product.name = 'Example Product'
+      expect(product.valid?).to be_truthy
+    end
+
   end  
 end
