8d3bc804acbbe544feca4c6108aed1fe635897b8
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-11 10:31:58 -0700

    update product tests, add remaining 3 required inputs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -27,5 +27,41 @@ RSpec.describe Product, type: :model do
       expect(product.valid?).to be_truthy
     end
 
+    it 'validates presence of price' do
+      product = Product.new(name: 'exampleP', quantity: 5, category: Category.new)
+
+      # Test the false path first (No price) which should be required by our model
+      expect(product.valid?).to be_falsey
+      expect(product.errors[:price]).to include("can't be blank")
+
+      # Test after valid price
+      product.price = 10
+      expect(product.valid?).to be_truthy
+    end
+
+    it 'validates presence of quantity' do
+      product = Product.new(name: 'exampleP', price: 10, category: Category.new)
+
+      # Test the false path first (No quantity) which should be required by our model
+      expect(product.valid?).to be_falsey
+      expect(product.errors[:quantity]).to include("can't be blank")
+
+      # Test after valid quantity
+      product.quantity = 5
+      expect(product.valid?).to be_truthy
+    end
+
+    it 'validates presence of category' do
+      product = Product.new(name: 'exampleP', price: 10, quantity: 5)
+
+      # Test the false path first (No category) which should be required by our model
+      expect(product.valid?).to be_falsey
+      expect(product.errors[:category]).to include("can't be blank")
+
+      # Test after valid quantity
+      product.category = Category.new
+      expect(product.valid?).to be_truthy
+    end
+
   end  
 end
