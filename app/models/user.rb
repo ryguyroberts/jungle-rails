1d17a2e5c7c44576b1a156b9873a970007c83e41
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-11 18:13:55 -0700

    add authencation method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,17 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  
+  validates :password, length: { minimum: 8 }
+
+  def self.authenticate_with_credentials(email, password)
+    # Find the user by email
+    user = User.find_by(email: email.strip.downcase)
+
+    # If the user exists and the password is correct, return the user
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,69 +14,115 @@ RSpec.describe User, type: :model do
       expect(user.valid?).to be_truthy
     end
 
-    # it 'is not valid if passwords are supplied but do not match' do
-    #   user = User.new(
-    #     name: 'TestUser',
-    #     email: 'testuser@gmail.com',
-    #     password: 'testpass',
-    #     password_confirmation: 'passtest'
-    #   )
+    it 'is not valid if passwords are supplied but do not match' do
+      user = User.new(
+        first_name: 'Test',
+        last_name: 'User',
+        email: 'testuser@gmail.com',
+        password: 'testpass',
+        password_confirmation: 'passtest'
+      )
 
-    #   expect(user.valid?).to be_falsey
-    # end
+      expect(user.valid?).to be_falsey
+    end
 
-    # it 'is not valid if the email is already in the DB' do
-    #   user = User.create(
-    #     name: 'TestUser',
-    #     email: 'testuser@gmail.com',
-    #     password: 'testpass',
-    #     password_confirmation: 'testpass'
-    #   )
+    it 'is not valid if the email is already in the DB' do
+      user = User.create(
+        first_name: 'Test',
+        last_name: 'User',
+        email: 'testuser@gmail.com',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      )
 
-    #   expect(user.valid?).to be_truthy
+      expect(user.valid?).to be_truthy
 
-    #   # Duplicate user with same email, but case difference
-    #   duplicate_user = User.new(
-    #     name: 'TestUser',
-    #     email: 'TESTUSER@gmail.com',
-    #     password: 'testpass',
-    #     password_confirmation: 'testpass'
-    #   )
+      # Duplicate user with same email, but case difference
+      duplicate_user = User.new(
+        first_name: 'Test',
+        last_name: 'User',
+        email: 'TESTUSER@gmail.com',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      )
 
-    #   expect(duplicate_user).not_to be_valid
-    #   expect(duplicate_user.errors[:email]).to include('has already been taken')
-    # end
+      expect(duplicate_user).not_to be_valid
+      expect(duplicate_user.errors[:email]).to include('has already been taken')
+    end
 
-    # it 'is not valid if no username was supplied' do
-    #   user = User.new(
-    #     email: 'testuser@gmail.com',
-    #     password: 'testpass',
-    #     password_confirmation: 'testpass'
-    #   )
+    it 'is not valid if no first name was supplied' do
+      user = User.new(
+        last_name: 'User',
+        email: 'testuser@gmail.com',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      )
 
-    #   #false path
-    #   expect(user.valid?).to be_falsey
-    #   expect(user.errors[:name]).to include("can't be blank")
+      #false path
+      expect(user.valid?).to be_falsey
+      expect(user.errors[:first_name]).to include("can't be blank")
 
-    #   #input the name now
-    #   user.name = 'TestUser'
-    #   expect(user.valid?).to be_truthy
-    # end
+      #input the name now
+      user.first_name = 'Test'
+      expect(user.valid?).to be_truthy
+    end
 
-    # it 'is not valid if no email was supplied' do
-    #   user = User.new(
-    #     name: 'TestUser',
-    #     password: 'testpass',
-    #     password_confirmation: 'testpass'
-    #   )
+    it 'is not valid if no last name was supplied' do
+      user = User.new(
+        first_name: 'Test',
+        email: 'testuser@gmail.com',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      )
 
-    #   #false path
-    #   expect(user.valid?).to be_falsey
-    #   expect(user.errors[:email]).to include("can't be blank")
+      #false path
+      expect(user.valid?).to be_falsey
+      expect(user.errors[:last_name]).to include("can't be blank")
 
-    #   #input the name now
-    #   user.name = 'TestUser'
-    #   expect(user.valid?).to be_truthy
-    # end
+      #input the name now
+      user.last_name = 'User'
+      expect(user.valid?).to be_truthy
+    end
+
+    it 'is not valid if no email was supplied' do
+      user = User.new(
+        first_name: 'Test',
+        last_name: 'User',
+        password: 'testpass',
+        password_confirmation: 'testpass'
+      )
+
+      #false path
+      expect(user.valid?).to be_falsey
+      expect(user.errors[:email]).to include("can't be blank")
+
+      #input the name now
+      user.email = 'testuser@gmail.com'
+      expect(user.valid?).to be_truthy
+    end
+
+    it 'is not valid if password is less than 8 chars.' do
+
+      # pass to short
+      user = User.new(
+        first_name: 'Test',
+        last_name: 'User',
+        email: 'testuser@gmail.com',
+        password: 'short',
+        password_confirmation: 'short'
+      )
+
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("is too short (minimum is 8 characters)")
+
+      # make pass longer
+      user.password = 'testpass'
+      user.password_confirmation = 'testpass'
+
+      expect(user.valid?).to be_truthy
+
+    end
   end
+
+  describe '.authenticate_with_credentials'
 end
