35ec47b9557f437cede00fe871792bfeb4d5c18a
Ryan Robertson <ryguyrobertson@gmail.com>
2024-03-12 14:55:57 -0700

    add second half of test to see if cart increments

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -13,8 +13,8 @@ describe('example to-do app', () => {
     // Find and click on the "Add to Cart" button
     cy.get(".products article").first().find("form.button_to button.btn").click({ force: true });
 
-    // Optionally, you can assert that the cart state is updated after clicking the button
-    // For example, you can check if the cart count increases or a message confirming the addition appears
+    // Test for 1 but
+    cy.get('a.nav-link[href="/cart"]').should('contain', `My Cart (1)`);
   });
 
 });
